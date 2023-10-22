029b3ef0a7f252fd274eac0629dfc39727f3515e
Chantal <chantalgomez@pursuit.org>
2023-10-22 10:33:42 -0400

    Updated list comp

--- wimzi-frontend/src/App.js
+++ wimzi-frontend/src/App.js
@@ -7,10 +7,10 @@ import './App.css'
 function App() {
   return (
     <div className="App"
-    // style={{backgroundImage: `url(${'https://d7hftxdivxxvm.cloudfront.net/?height=528&quality=85&resize_to=fit&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FpoQnSnAxjupoTUtNALBq8g%2Fnormalized.jpg&width=800'})`,
-    // backgroundRepeat: "no-repeat",
-    // backgroundSize: "cover",
-    // backgroundColor: '#FFFFFF50'
+  //   style={{backgroundImage: `url(${'https://d7hftxdivxxvm.cloudfront.net/?height=528&quality=85&resize_to=fit&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FpoQnSnAxjupoTUtNALBq8g%2Fnormalized.jpg&width=800'})`,
+  //   backgroundRepeat: "no-repeat",
+  //   backgroundSize: "cover",
+  //   backgroundColor: '#FFFFFF50'
   // }}
   //------------------could not do the image background with opacity down 
     >
--- wimzi-frontend/src/Componets/Lists/Lists.js
+++ wimzi-frontend/src/Componets/Lists/Lists.js
@@ -3,32 +3,70 @@ import "./Lists.css";
 
 function Lists() {
   return (
-    <div className='list-container'>
-    <ul className="list-group">
-    {/* <li className="list-group-item d-flex justify-content-between align-items-center">
-        <button className="list-group-item d-flex justify-content-between align-items-center">
-        Add
-        </button>
-  </li> */}
-  <button className="list-group-item d-flex justify-content-between align-items-center list-group-item-info">
-        Add New Reminder
-        </button>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
-    A list item
-    <span className="badge bg-info  rounded-pill">14</span>
-  </li>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
-    A second list item
-    <span className="badge bg-info rounded-pill">2</span>
-  </li>
-  <li className="list-group-item d-flex justify-content-between align-items-center">
-    A third list item
-    <span className="badge bg-info  rounded-pill">1</span>
-  </li>
-</ul>
-<button className='button-52'>
-Submit
-</button>
+//     <div className='list-container'>
+//     <ul className="list-group">
+//     <li className="list-group-item d-flex justify-content-between align-items-center">
+//         <button className="list-group-item d-flex justify-content-between align-items-center">
+//         Add
+//         </button>
+//   </li>
+//   <button className="list-group-item d-flex justify-content-between align-items-center list-group-item-info">
+//         Add New Reminder
+//         </button>
+//   <li className="list-group-item d-flex justify-content-between align-items-center">
+//     A list item
+//     <span className="badge bg-info  rounded-pill">14</span>
+//   </li>
+//   <li className="list-group-item d-flex justify-content-between align-items-center">
+//     A second list item
+//     <span className="badge bg-info rounded-pill">2</span>
+//   </li>
+//   <li className="list-group-item d-flex justify-content-between align-items-center">
+//     A third list item
+//     <span className="badge bg-info  rounded-pill">1</span>
+//   </li>
+// </ul>
+// <button className='button-52'>
+// Submit
+// </button>
+// </div>
+<div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Accordion Item #2
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Accordion Item #3
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
 </div>
   )
 }
