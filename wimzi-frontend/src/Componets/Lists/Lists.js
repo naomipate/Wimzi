829f93b6a2d713f771f41547da24f0469b56f205
ambarimar ulloa <ambarimar@ambarimars-MacBook-Air.local>
2023-10-22 14:12:46 -0400

    added to about and fixed nav

--- wimzi-frontend/src/Componets/Lists/Lists.js
+++ wimzi-frontend/src/Componets/Lists/Lists.js
@@ -1,48 +1,84 @@
-import React from 'react'
-import SubLists from './SubLists';
-import "./Lists.css"; 
+import React from "react";
+import SubLists from "./SubLists";
+import "./Lists.css";
 
 function Lists() {
   return (
-<div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button bg-info" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        New Reminder
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-      <form className='input-submit'>
-  <div class="mb-3">
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-  </div>
-  <button type="submit" class="btn btn-info">+</button>
-</form>
+    <div class="accordion" id="accordionExample">
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button
+            class="accordion-button bg-info"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseOne"
+            aria-expanded="true"
+            aria-controls="collapseOne"
+          >
+            New Reminder
+          </button>
+        </h2>
+        <div
+          id="collapseOne"
+          class="accordion-collapse collapse show"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+            <form className="input-submit">
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                />
+              </div>
+              <button type="submit" class="btn btn-info">
+                +
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button
+            class="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseTwo"
+            aria-expanded="false"
+            aria-controls="collapseTwo"
+          >
+            Accordion Item #2
+          </button>
+        </h2>
+        <div
+          id="collapseTwo"
+          class="accordion-collapse collapse"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+            <form className="input-submit">
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                />
+              </div>
+              <button type="submit" class="btn btn-info">
+                +
+              </button>
+            </form>
+
+            <SubLists />
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-      <form className='input-submit'>
-  <div class="mb-3">
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-  </div>
-  <button type="submit" class="btn btn-info">+</button>
-</form>
-
-<SubLists/>
-    </div>
-    </div>
-  </div>
-</div>
-  )
+  );
 }
 
-export default Lists
+export default Lists;
--- wimzi-frontend/src/Componets/Nav/Nav.js
+++ wimzi-frontend/src/Componets/Nav/Nav.js
@@ -4,11 +4,16 @@ import "./Nav.css";
 function Nav() {
   return (
     <nav className="navbar bg-dark border-bottom border-body">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Wimzi</a>
-  </div>
-</nav>
-  )
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Wimzi
+        </a>
+        <a className="navbar-brand" href="/about">
+          About
+        </a>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav
