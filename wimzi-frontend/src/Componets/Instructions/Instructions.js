123eb9c4f3617d1e16167f7f080f7ad462e04f9e
Naomi-Pate <patenaomi99@gmail.com>
2023-10-22 10:42:34 -0400

    fixed merge

---
+++ wimzi-frontend/src/Componets/Instructions/Instructions.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Instructions.css"; 
+
+function Instructions() {
+  return (
+
+        <div className="card border-info mb-3" 
+        // style={{maxWidth: "18rem"}}
+        >
+    <div className="card-header">Instructions</div>
+    <div className="card-body">
+      <h5 className="card-title"></h5>
+      <p className="card-text">This website is for artist who need a daily does of inspiration.
+      Enjoy the art of the day. More info about the art is in the widget below. 
+      <hr/>
+      Make reminders and have then easily imported into you google calender once you submit.</p>
+    </div>
+    </div> 
+  )
+}
+
+export default Instructions
---
+++ wimzi-frontend/src/Componets/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Nav.css"; 
+
+function Nav() {
+  return (
+    <nav className="navbar bg-dark border-bottom border-body">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Wimzi</a>
+  </div>
+</nav>
+  )
+}
+
+export default Nav
