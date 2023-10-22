526a299948836d826e0e813f6098f60f1c2b69d6
Chantal <chantalgomez@pursuit.org>
2023-10-22 12:00:39 -0400

    Lists comp updated and SubList comp made

--- wimzi-frontend/src/Componets/Lists/Lists.js
+++ wimzi-frontend/src/Componets/Lists/Lists.js
@@ -1,46 +1,24 @@
 import React from 'react'
+import SubLists from './SubLists';
 import "./Lists.css"; 
 
 function Lists() {
   return (
-//     <div className='list-container'>
-//     <ul className="list-group">
-//     <li className="list-group-item d-flex justify-content-between align-items-center">
-//         <button className="list-group-item d-flex justify-content-between align-items-center">
-//         Add
-//         </button>
-//   </li>
-//   <button className="list-group-item d-flex justify-content-between align-items-center list-group-item-info">
-//         Add New Reminder
-//         </button>
-//   <li className="list-group-item d-flex justify-content-between align-items-center">
-//     A list item
-//     <span className="badge bg-info  rounded-pill">14</span>
-//   </li>
-//   <li className="list-group-item d-flex justify-content-between align-items-center">
-//     A second list item
-//     <span className="badge bg-info rounded-pill">2</span>
-//   </li>
-//   <li className="list-group-item d-flex justify-content-between align-items-center">
-//     A third list item
-//     <span className="badge bg-info  rounded-pill">1</span>
-//   </li>
-// </ul>
-// <button className='button-52'>
-// Submit
-// </button>
-// </div>
 <div class="accordion" id="accordionExample">
   <div class="accordion-item">
     <h2 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+      <button class="accordion-button bg-info" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
         New Reminder
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-      <strong>This is the secondd item's accordion body.</strong>
-        <p>message</p>
+      <form className='input-submit'>
+  <div class="mb-3">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+  </div>
+  <button type="submit" class="btn btn-info">+</button>
+</form>
       </div>
     </div>
   </div>
@@ -52,22 +30,15 @@ function Lists() {
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-      <strong>This is the secondd item's accordion body.</strong>
-        <p>message</p>
-    </div>
-    </div>
+      <form className='input-submit'>
+  <div class="mb-3">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
   </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong>
-        <p>message</p>
-      </div>
+  <button type="submit" class="btn btn-info">+</button>
+</form>
+
+<SubLists/>
+    </div>
     </div>
   </div>
 </div>
---
+++ wimzi-frontend/src/Componets/Lists/SubLists.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./SubLists.css"; 
+
+function SubLists() {
+  return (
+    <ul class="list-group list-group-flush">
+  <li class="list-group-item">
+  <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckboxStretched"/>
+    An item</li>
+    <li class="list-group-item">
+  <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckboxStretched"/>
+    An item</li>
+</ul>
+  )
+}
+
+export default SubLists
