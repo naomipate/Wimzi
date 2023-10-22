71decf56290103e9dfe146a4ee78ac1eb1de81ca
AkiraBrown <akira.watson.brown@gmail.com>
2023-10-22 13:30:27 -0400

    ImageComp has Api

--- wimzi-frontend/src/Componets/ImageComp/ImageComp.js
+++ wimzi-frontend/src/Componets/ImageComp/ImageComp.js
@@ -1,16 +1,40 @@
-import React from 'react'
-import "./ImageComp.css"; 
-
+import { useState, useEffect } from "react";
+import "./ImageComp.css";
+import { getImageById } from "../../API/API";
 
 function ImageComp() {
+  const [imageDesc, setImageDesc] = useState("");
+  const [imgSrc, setImgSrc] = useState("");
+
+  useEffect(() => {
+    fetchImage();
+  }, []);
+
+  async function fetchImage() {
+    const randImg = Math.floor(Math.random() * 50);
+    // console.log(randImg);
+    let data = await getImageById(randImg);
+    // console.log(data);
+    // console.log(data.data[0].image_id);
+    setImgSrc(
+      `https://www.artic.edu/iiif/2/${
+        data.data[0].image_id
+          ? data.data[0].image_id
+          : "2ffde7b6-d57c-eba4-c6af-9f4e1dc91d72"
+      }/full/843,/0/default.jpg`
+    );
+    // console.log(data.data[0].title);
+    setImageDesc(data.data[0].title);
+  }
+
   return (
-<div className="card" style={{width: "450px"}}>
-  <img src="https://d7hftxdivxxvm.cloudfront.net/?height=528&quality=85&resize_to=fit&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FpoQnSnAxjupoTUtNALBq8g%2Fnormalized.jpg&width=800" className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-  )
+    <div className="card" style={{ width: "450px" }}>
+      <img src={imgSrc} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <p className="card-text">{imageDesc}</p>
+      </div>
+    </div>
+  );
 }
 
-export default ImageComp
+export default ImageComp;
--- wimzi-frontend/src/Componets/Nav/Nav.js
+++ wimzi-frontend/src/Componets/Nav/Nav.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import "./Nav.css"; 
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Nav.css";
 
 function Nav() {
   return (
     <nav className="navbar bg-dark border-bottom border-body">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Wimzi</a>
-  </div>
-</nav>
-  )
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to={"/"}>
+          Wimzi
+        </NavLink>
+        <NavLink>About</NavLink>
+      </div>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
