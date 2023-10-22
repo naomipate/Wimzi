6cd66d7235e9699388e3cc49bcb5227d6a1872db
Chantal <chantalgomez@pursuit.org>
2023-10-22 10:53:55 -0400

    committing changes before merge

--- wimzi-frontend/src/Componets/Lists/Lists.js
+++ wimzi-frontend/src/Componets/Lists/Lists.js
@@ -34,12 +34,13 @@ function Lists() {
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
+        New Reminder
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      <strong>This is the secondd item's accordion body.</strong>
+        <p>message</p>
       </div>
     </div>
   </div>
@@ -51,8 +52,9 @@ function Lists() {
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
+      <strong>This is the secondd item's accordion body.</strong>
+        <p>message</p>
+    </div>
     </div>
   </div>
   <div class="accordion-item">
@@ -63,7 +65,8 @@ function Lists() {
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        <strong>This is the third item's accordion body.</strong>
+        <p>message</p>
       </div>
     </div>
   </div>
